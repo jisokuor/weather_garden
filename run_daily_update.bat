d935ff21370646da682255ce1966f8ca51876ef4
Jarkko Iso-Kuortti <jarkko.isok@gmail.com>
2024-05-31 05:43:55 +0300

    Daily weather and gardening tips update

--- run_daily_update.bat
+++ run_daily_update.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d G:\Pycharm\Pycharmprojects\Agents
 
+echo Activating virtual environment
+call G:\Pycharm\Pycharmprojects\Agents\.venv\Scripts\activate.bat
 
 echo Running Python script
 G:\Pycharm\Pycharmprojects\Agents\.venv\Scripts\python.exe openweather_api_test7.py
