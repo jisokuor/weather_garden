87bb4cd788fc3a314928bddbef63200ba278b456
Jarkko Iso-Kuortti <jarkko.isok@gmail.com>
2024-05-31 04:24:18 +0300

    Update from openweather_api_test7.py

--- run_daily_update.bat
+++ run_daily_update.bat
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 cd /d G:\Pycharm\Pycharmprojects\Agents
 
 echo Activating virtual environment
@@ -12,21 +12,24 @@ G:\Pycharm\Pycharmprojects\Agents\.venv\Scripts\python.exe -m pip list
 
 python.exe -m pip install --upgrade pip
 
-echo Switching to gh-pages  branch
-git checkout gh-pages
-
 echo Running Python script
 G:\Pycharm\Pycharmprojects\Agents\.venv\Scripts\python.exe openweather_api_test7.py
 
-REM Pulling latest changes from main
+REM Updating gh-pages branch
+echo Switching to gh-pages branch
+git checkout gh-pages
 git pull origin gh-pages --rebase
-
-REM Adding and committing changes
 git add -A
-git commit -m "Daily weather and gardening tips update"
-
-REM Pushing changes to main
+git commit -m "Update from openweather_api_test7.py"
 git push origin gh-pages
+
+REM Updating main branch
+echo Switching to main branch
+git checkout main
+git pull origin main --rebase
+git add -A
+git commit -m "Update from openweather_api_test7.py"
+git push origin main
 
 echo Cleaning up Git repository
 git gc --prune=now
